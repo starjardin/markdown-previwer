d9ef5babb0355570ece25fd5358ef683bd93790d
Tantely Andrianarivola <tantely.and@onja.org>
2020-11-03 07:45:58 +0300

    added calculator machine

---
+++ components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Keys from "./Keys"
+
+
+export default function App () {
+  return (
+    <Keys ></Keys>
+  )
+}
---
+++ components/Keys.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+export default function Keys () {
+  return (
+    <>
+      <div className="keys">
+        <div className="key ac">AC</div>
+        <div className="key division">/</div>
+        <div className="key times">x</div>
+        <div className="key seven">7</div>
+        <div className="key eight">8</div>
+        <div className="key nine">9</div>
+        <div className="key menus">-</div>
+        <div className="key four">4</div>
+        <div className="key five">5</div>
+        <div className="key sixe">6</div>
+        <div className="key plus">+</div>
+        <div className="key one">1</div>
+        <div className="key two">2</div>
+        <div className="key three">3</div>
+        <div className="key equal">=</div>
+        <div className="key zero">0</div>
+        <div className="key dot">.</div>
+      </div>
+    </>
+  )
+}
